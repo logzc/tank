17ec1ca2c1b1af2d7ba06002e8e65d90ef590296
zicla <ziclax@126.com>
2017-09-06 18:32:04 +0800

    Remove useless things.

--- rest/user/controller.go
+++ rest/user/controller.go
@@ -2,8 +2,6 @@ package user
 
 import (
 	"net/http"
-	"fmt"
-	"strings"
 	"tank/result"
 )
 
@@ -12,17 +10,6 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	email := r.FormValue("email")
 	password := r.FormValue("password")
 
-	fmt.Println(email + ":" + password)
-
-	r.ParseForm()
-	fmt.Println(r.Form)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
 	if "" == email || "" == password {
 		result.OutputError(w, "请输入邮箱和密码")
 		return
@@ -34,7 +21,6 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		result.OutputError(w, "邮箱或密码错误")
 		return
 	} else {
-
 		if user.Password != password {
 			result.OutputError(w, "邮箱或密码错误")
 			return
